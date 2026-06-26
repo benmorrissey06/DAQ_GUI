62e655dea9d9a9f72be66b184e3213ea9157bc7d
bmorrissey9 <bmorrissey9@gatech.edu>
2026-06-26 15:02:30 -0400

    Added recording control and segment features to gui.py. TO DO: daq.py serial communication functions, TTL, input/output triggers, working filepath

--- gui/gui.py
+++ gui/gui.py
@@ -2,21 +2,34 @@ import dearpygui.dearpygui as dpg
 import threading
 import time
 #from daq import DAQController  (Will implement tis later)
-
+'''
+currently commenting out all DAQ references so we can test GUI before that is finished.
+'''
 VIS_PD_MAX = 255
 IR_PD_MAX = 255
 VIS_LED_MAX = 4095
-
 
 class GUI:
     def __init__(self):
         #self.daq = DAQController()
         self.is_live = False
         self.running = True
+
+        self.file_path = ""
         
         self.x_data = []
         self.y_data = []
         self.max_points = 5000 
+
+        self.light_values = []
+        self.time_stamps = []
+        self.recording_duration = 0.0
+        self.active_rows = []
+
+        #When implementing custom recording controls, this labels each segment of time
+        self.segment_counter = 1
+        #Whether or not there is an overlap in the segments, will be function to determine
+        self.no_overlap = True
         
         dpg.create_context()
         self.build_ui()
@@ -45,6 +58,70 @@ class GUI:
         else:
             dpg.configure_item("plot_ir", show=False)
             dpg.configure_item("plot_vis", show=False)
+
+    def start_recording(self,sender,app_data,user_data):
+        pass
+        #self.daq.begin_recording()
+    
+    def store_segment(self,sender,app_data,user_data):
+        self.time_stamps.clear()
+        self.light_values.clear()
+        
+        for row_id in self.active_rows:
+            start_val = dpg.get_value(f"start_{row_id}")
+            end_val = dpg.get_value(f"end_{row_id}")
+            uv_val = dpg.get_value(f"uv_{row_id}")
+            
+            self.time_stamps.append((start_val, end_val))
+            self.light_values.append(uv_val)
+    
+    def update_recording_duration(self, sender, app_data, user_data):
+        self.recording_duration = app_data
+
+    def delete_segment(self, sender, app_data, user_data):
+        #delete when pressing the X button
+        if user_data in self.active_rows:
+            self.active_rows.remove(user_data)
+        dpg.delete_item(user_data)
+    
+
+    def add_recording_segment(self, app_data, user_data):
+        #add a new segment under custom recording controls
+        self.segment_counter+=1
+        row_tag = f"row_{self.segment_counter}"
+        self.active_rows.append(row_tag)
+
+        with dpg.group(horizontal=True, parent="protocol_group", tag=row_tag):
+            dpg.add_separator()
+            dpg.add_input_int(label="Start (s)", width=100, step=0, tag=f"start_{self.segment_counter}",callback = self.store_segment)
+            dpg.add_input_int(label="End (s)", width=100, step=0, tag=f"end_{self.segment_counter}",callback =self.store_segment)
+            dpg.add_input_int(label="UV Val", width=100, step=0, tag=f"uv_{self.segment_counter}",callback = self.store_segment)
+        
+        
+            dpg.add_button(label=" X ", user_data=row_tag, callback=self.delete_segment)
+
+    def prepare_recording(self):
+        dpg.delete_item("Confirm info", children_only=True)
+        self.time_stamps.clear()
+        self.light_values.clear()
+
+        for row_id in self.active_rows:
+            row_num = row_id.split("_")[-1]
+            start_val = dpg.get_value(f"start_{row_num}")
+            end_val = dpg.get_value(f"end_{row_num}")
+            uv_val = dpg.get_value(f"uv_{row_num}")
+            self.time_stamps.append((start_val, end_val))
+            self.light_values.append(uv_val)
+
+        if self.no_overlap:
+            dpg.add_text(f"Recording Duration: {self.recording_duration}", parent="Confirm info")
+            for i in range(len(self.light_values)):
+                dpg.add_text(
+                    f"Segment {i+1}: Value of {self.light_values[i]} from time {self.time_stamps[i][0]} s to time {self.time_stamps[i][1]} s",
+                    parent="Confirm info"
+                )
+        else:
+            dpg.add_text("Error - overlapping time segments", parent="Confirm info")
 
 
     def slider_callback(self, sender, app_data,user_data):
@@ -76,16 +153,31 @@ class GUI:
                             dpg.add_spacer(height=10)
                             dpg.add_separator()
                             dpg.add_spacer(height=10)
-                            dpg.add_slider_int(label="IR LED Blink", max_value=1000, width=200) 
-                            dpg.add_slider_int(label="VIS PD Gain", max_value=VIS_PD_MAX, width=200) 
-                            dpg.add_slider_int(label="IR PD Gain", max_value=IR_PD_MAX, width=200) 
-                            dpg.add_slider_int(label="VIS LED Gain", max_value=VIS_LED_MAX, width=200) 
+                            dpg.add_slider_int(label="IR LED Blink", max_value=1000, width=200, callback=self.slider_callback) 
+                            dpg.add_slider_int(label="VIS PD Gain", max_value=VIS_PD_MAX, width=200, callback=self.slider_callback) 
+                            dpg.add_slider_int(label="IR PD Gain", max_value=IR_PD_MAX, width=200, callback=self.slider_callback) 
+                            dpg.add_slider_int(label="VIS LED Gain", max_value=VIS_LED_MAX, width=200, callback=self.slider_callback) 
                         #One tab here which has all these automation controls
-                        with dpg.tab(label="Automation"):
+                        with dpg.tab(label="Recording Options"):
                             dpg.add_spacer(height=10)
-                            dpg.add_text("Timing Controls")
+                            dpg.add_text("Start Recording: ")
                             dpg.add_separator()
-                            dpg.add_spacer(height=30)
+                            dpg.add_input_float(label = "Duration (s)", width = 200, callback=self.update_recording_duration)
+                            dpg.add_spacer(height=10)
+                            dpg.add_text("Add Recording Segments: ")
+                            dpg.add_separator()
+                            dpg.add_button(label="Add Segment", callback=self.add_recording_segment)
+                            # The group will then appear here
+                            with dpg.group(tag="protocol_group"):
+                                dpg.add_text("")
+                            dpg.add_button(label = "Save Recording Settings",callback=self.prepare_recording)
+                            with dpg.group(tag = "Confirm info"):
+                                dpg.add_text("")
+                            dpg.add_spacer(height=10)
+                            dpg.add_input_text(label = "File path",tag = "filepath")
+                            self.file_path=dpg.get_value("filepath") #Save filepath, maybe have file expolorer open instead?
+                            dpg.add_button(label="START",callback=self.start_recording)
+                            dpg.add_separator()
                             dpg.add_text("TTL Outputs")
                             dpg.add_separator()
 
